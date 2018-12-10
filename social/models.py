77cba2edb4aff29b5b06eb39806cb35f300040ad
danielmessi13 <danielmessi13@hotmail.com>
2018-12-10 01:24:11 -0300

    Atualizado

--- social/models.py
+++ social/models.py
@@ -14,6 +14,7 @@ class Usuario(models.Model):
     senha = models.CharField(max_length=50)
     tipo = models.CharField(max_length=10, choices=TIPOS)
     foto = models.ImageField(null=True)
+    amigos = models.ManyToManyField('Usuario')
 
     def __str__(self):
         return self.nome
@@ -27,7 +28,7 @@ class Anexo(models.Model):
 
     arquivo = models.FileField(upload_to='static')
     tipo = models.CharField(max_length=30, choices=TIPOS)
-    anexo = models.ForeignKey('Postagem', related_name='anexo_postagem', on_delete=models.CASCADE, null=True, blank=True)
+    postagem = models.ForeignKey('Postagem', related_name='anexo_postagem', on_delete=models.CASCADE, null=True, blank=True)
 
 
 class Questao(models.Model):
@@ -76,3 +77,12 @@ class Postagem(models.Model):
     data = models.DateField(timezone.now)
     usuario = models.ForeignKey(Usuario, related_name='usuario_postagem', on_delete=models.CASCADE)
     questao = models.OneToOneField(Questao, related_name='questao_postagem', on_delete=models.CASCADE, null=True, blank=True)
+
+class Convite(models.Model):
+    convidado = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='convites_recebidos')
+    solicitante = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='convites_feitos')
+
+    def aceitar(self):
+        self.convidado.contatos.add(self.solicitante)
+        self.solicitante.contatos.add(self.convidado)
+        self.delete()
